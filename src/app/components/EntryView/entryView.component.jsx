60e359d462da40a021f159d9e96c31b74d8bc380
Momoh Nobert <okhai0112@gmail.com>
2021-03-16 12:47:18 +0100

    created entry view component

---
+++ src/app/components/EntryView/entryView.component.jsx
@@ -0,0 +1,34 @@
+import { EVStyle } from "./entryView.styles";
+import { TweenLite } from 'gsap';
+import { useRef } from 'react';
+
+const EntryView = () => {
+    let imgRef = useRef(null);
+    
+
+    function handleMouse(e) {
+        const { offsetX, offsetY, target } = e.nativeEvent;
+        const { clientWidth, clientHeight } = target;
+
+        const xPos = (offsetX/clientWidth) - 0.5;
+        const yPos = (offsetY/clientHeight) - 0.5;
+
+        console.log(xPos, yPos)
+
+        TweenLite.to(imgRef, {
+            duration: 1.2,
+            x: xPos*100,
+            y: yPos*100,
+            rotationY: xPos*40,
+            rotationX: yPos*20
+        })
+    }
+
+    return (
+        <EVStyle onMouseMove={handleMouse}>
+            <img src="/map.jpg" alt="map" height={500} width={700} ref={el => imgRef = el} />
+        </EVStyle>
+    )
+}
+
+export default EntryView
---
+++ src/app/components/EntryView/entryView.styles.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const EVStyle = styled.div`
+    min-height: 100vh;
+    width: 100vw;
+    background-color: rgba(0,0,0,0.1);
+    display: grid;
+    place-items: center;
+    perspective: 1000px;
+    
+    img {
+        box-shadow: 0px 5px 10px rgba(0,0,0,0.5);
+    }
+`
