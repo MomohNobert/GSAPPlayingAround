e77a679f2bbb88f8f2363f8510a2c9030dc698af
Momoh Nobert <okhai0112@gmail.com>
2021-03-17 09:49:08 +0100

    added linting

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
-import EntryView from "../app/components/EntryView/entryView.component";
+import EntryView from '../app/components/EntryView/entryView.component';
 
 export default function Home() {
   return (
     <div>
       <EntryView />
     </div>
-  )
+  );
 }
