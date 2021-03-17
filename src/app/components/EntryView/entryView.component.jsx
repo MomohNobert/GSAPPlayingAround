4bc33e9c6f337d78acafee2dada8ce0d6080bd4e
MomohNobert <okhai0112@gmail.com>
2021-03-17 13:13:28 +0100

    reverting to old format

--- src/app/components/EntryView/entryView.component.jsx
+++ src/app/components/EntryView/entryView.component.jsx
@@ -1,7 +1,7 @@
 import { EVStyle } from "./entryView.styles";
-import gsap, { TimelineMax } from 'gsap';
-import { useRef, useEffect } from 'react';
-import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import TweenLite from 'gsap';
+import { useRef} from 'react';
+// import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 
 const EntryView = () => {
     let imgRef = useRef(null);
@@ -15,47 +15,47 @@ const EntryView = () => {
     //     }
     // })
 
-    useEffect(() => {
-        if (typeof window !== "undefined") {
-            gsap.registerPlugin(ScrollTrigger); 
-        }
-        // tl.to(imgRef, {
-        //     duration: 1.5,
-        //     x: 100,
-        //     y: 100,
-        // })
+    // useEffect(() => {
+    //     if (typeof window !== "undefined") {
+    //         gsap.registerPlugin(ScrollTrigger); 
+    //     }
+    //     // tl.to(imgRef, {
+    //     //     duration: 1.5,
+    //     //     x: 100,
+    //     //     y: 100,
+    //     // })
 
-        ScrollTrigger.create({
-            trigger: imgRef.current,
-            start: "100px",
-            end: "700px",
-            markers: true,
-            onToggle: self => console.log("toggled, isActive:", self.isActive),
-            onUpdate: self => {
-                console.log("progress:", self.progress.toFixed(3), "direction:", self.direction, "velocity", self.getVelocity());
-            }
-            });
-    }, [])
+    //     ScrollTrigger.create({
+    //         trigger: imgRef.current,
+    //         start: "100px",
+    //         end: "700px",
+    //         markers: true,
+    //         onToggle: self => console.log("toggled, isActive:", self.isActive),
+    //         onUpdate: self => {
+    //             console.log("progress:", self.progress.toFixed(3), "direction:", self.direction, "velocity", self.getVelocity());
+    //         }
+    //         });
+    // }, [])
 
-    // function handleMouse(e) {
-    //     const { offsetX, offsetY, target } = e.nativeEvent;
-    //     const { clientWidth, clientHeight } = target;
+    function handleMouse(e) {
+        const { offsetX, offsetY, target } = e.nativeEvent;
+        const { clientWidth, clientHeight } = target;
 
-    //     const xPos = (offsetX/clientWidth) - 0.5;
-    //     const yPos = (offsetY/clientHeight) - 0.5;
+        const xPos = (offsetX / clientWidth) - 0.5;
+        const yPos = (offsetY / clientHeight) - 0.5;
 
-    //     TweenLite.to(imgRef, {
-    //         duration: 1.5,
-    //         x: xPos*100,
-    //         y: yPos*100,
-    //         rotationY: xPos*10,
-    //         rotationX: yPos*5
-    //     })
+        TweenLite.to(imgRef, {
+            duration: 1.5,
+            x: xPos * 100,
+            y: yPos * 100,
+            rotationY: xPos * 10,
+            rotationX: yPos * 5
+        })
 
-    // }
+    }
 
     return (
-        <EVStyle>
+        <EVStyle onMouseMove={handleMouse}>
             <img src="/map.jpg" alt="map" height={500} width={700} ref={el => imgRef = el} />
         </EVStyle>
     )
--- src/app/components/EntryView/entryView.styles.jsx
+++ src/app/components/EntryView/entryView.styles.jsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 export const EVStyle = styled.div`
-    min-height: 200vh;
+    min-height: 100vh;
     width: 100vw;
     background-color: rgba(0,0,0,0.1);
     display: grid;
